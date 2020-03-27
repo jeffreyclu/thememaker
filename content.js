e7ecf85f6894dc8adc95520edae82e5bd3c5f3c4
Jeffrey C. Lu <ceeloo@gmail.com>
2020-03-27 10:48:49 -0400

    refined color generator

--- content.js
+++ content.js
@@ -1,30 +1,8 @@
-//empty array to hold generate colors
-const generatedColors = [];
-//array of all basic html elements
-const htmlElements = ['body', 'ul', 'div', 'hr', 'span', 'button', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'p'];
-//array of different modes
-const modes = ['monochrome', 'monochrome-dark', 'monochrome-light', 'analogic', 'complement', 'analogic-complement', 'triad', 'quad'];
-//generate a random mode to put into fetch request
-const randomMode = modes[randomNum(0, modes.length-1)];
-//generate a random color to put into fetch request
-const randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-fetch(`http://thecolorapi.com/scheme?hex=${randomColor}&mode=${randomMode}&format=json&count=14`)
-    .then(data=>data.json())
-    .then(data=>{
-        console.log(data)
-        const colorArray = data["colors"];
-        colorArray.forEach(colorObj=>{
-            generatedColors.push(colorObj["hex"]["value"]);
-        })
-        const colorScheme = generateScheme();
-        console.log(colorScheme);
-        applyScheme(colorScheme);
-})
 //function to generate random number
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 //function to generate the color scheme object
 function generateScheme() {
     //empty object to hold color scheme -> {'body': 'color1', 'p': 'color2', etc.}
@@ -35,8 +13,7 @@ function generateScheme() {
     return colorScheme;
 }
 
-// use a function here:
-// document.querySelectorAll(each html element) set the element.style.color = each random color
+//function to apply the color scheme
 function applyScheme(schemeObj) {
     for (const [key, value] of Object.entries(schemeObj)) {
         if (key === 'body') {
@@ -61,3 +38,30 @@ function applyScheme(schemeObj) {
         
     }
 }
+
+//empty array to hold generate colors
+const generatedColors = [];
+//array of all basic html elements
+const htmlElements = ['body', 'ul', 'div', 'hr', 'span', 'button', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+//array of different modes
+const modes = ['monochrome', 'monochrome-dark', 'monochrome-light', 'analogic', 'complement', 'analogic-complement', 'triad', 'quad'];
+//generate a random mode to put into fetch request
+const randomMode = modes[randomNum(0, modes.length-1)];
+//generate a random color to put into fetch request
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+fetch(`http://thecolorapi.com/scheme?hex=${randomColor}&mode=${randomMode}&format=json&count=14`)
+    .then(data=>data.json())
+    .then(data=>{
+        console.log(data)
+        const colorArray = data["colors"];
+        colorArray.forEach(colorObj=>{
+            generatedColors.push(colorObj["hex"]["value"]);
+        })
+        const colorScheme = generateScheme();
+        console.log(colorScheme);
+        applyScheme(colorScheme);
+})
+
+
+
