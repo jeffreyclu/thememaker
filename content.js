7780933a150e8d948bc2471b4da5da994ed37856
Jeffrey C. Lu <ceeloo@gmail.com>
2020-03-30 17:33:05 -0400

    added details view

--- content.js
+++ content.js
@@ -29,6 +29,34 @@ resetButton.style.zIndex = "99999999";
 resetButton.onclick = resetScheme;
 docBody.appendChild(resetButton);
 
+const showDetailsButton = document.createElement("button");
+showDetailsButton.innerText = "show details"
+showDetailsButton.style.position = "fixed";
+showDetailsButton.style.top = "60px";
+showDetailsButton.style.right = "0px";
+showDetailsButton.style.zIndex = "99999999";
+showDetailsButton.onclick = showDetails;
+docBody.appendChild(showDetailsButton);
+
+const hideDetailsButton = document.createElement("button");
+hideDetailsButton.innerText = "hide details"
+hideDetailsButton.style.position = "fixed";
+hideDetailsButton.style.top = "80px";
+hideDetailsButton.style.right = "0px";
+hideDetailsButton.style.zIndex = "99999999";
+hideDetailsButton.onclick = hideDetails;
+docBody.appendChild(hideDetailsButton);
+
+const schemeDetails = document.createElement("div")
+schemeDetails.style.position = "fixed";
+schemeDetails.style.bottom = "0px";
+schemeDetails.style.right = "0px";
+schemeDetails.style.padding = "10px";
+schemeDetails.style.border = "1px solid black"
+schemeDetails.style.zIndex = "99999999";
+schemeDetails.style.display = "none";
+docBody.appendChild(schemeDetails);
+
 /**********the stuff above is just making the placeholder UI buttons************/
 
 
@@ -51,7 +79,14 @@ function generateScheme(colorArr) {
     for (let j = 0; j < htmlElements.length; j++) {
         colorScheme[htmlElements[j]] = colorArr[j];
     }
-    
+    //add color scheme details to DOM
+    let details = colorSchemeLink;
+    for (let [key, value] of Object.entries(colorScheme)) {
+        let newP = `<p>${key}: ${value}</p>`;
+        details += newP;
+    }
+    schemeDetails.innerHTML = details;
+    schemeDetails.style.background = "none"
     return colorScheme;
 }
 
@@ -87,6 +122,7 @@ const htmlElements = ['body', 'ul', 'div', 'hr', 'span', 'button', 'a', 'h1', 'h
 //global const array of different modes for fetch request
 const modes = ['monochrome', 'monochrome-dark', 'monochrome-light', 'analogic', 'complement', 'analogic-complement', 'triad', 'quad'];
 let colorSchemeGlobal = {};
+let colorSchemeLink;
 
 //function to fetch new color data
 function fetchData() {
@@ -96,7 +132,9 @@ function fetchData() {
     const randomMode = modes[randomNum(0, modes.length-1)];
     //generate a random color to put into fetch request
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    console.log('fetching', randomMode, randomColor)
+    console.log('fetching', randomMode, randomColor);
+    colorSchemeLink = `<p>Root Color: <a href="//www.thecolorapi.com/scheme?hex=${randomColor}&mode=${randomMode}&format=html&count=14" target="_blank">${randomColor}</a></p>`
+
     fetch(`//www.thecolorapi.com/scheme?hex=${randomColor}&mode=${randomMode}&format=json&count=14`)
     .then(data=>data.json())
     .then(data=>{
@@ -134,4 +172,14 @@ window.onload = function() {
     }
 }
 
+//function to display the color scheme details
+function showDetails() {
+    schemeDetails.style.display = "flex";
+    schemeDetails.style.flexFlow = "column";
+}
+
+//function to hide the color scheme details
+function hideDetails() {
+    schemeDetails.style.display = "none";
+}
 
