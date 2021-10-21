22b6cd28c856f12bfb08b151bba5a0f544b5d30e
jeffreyclu <hi@jeffreyclu.com>
2021-10-21 10:51:30 -0500

    add more mocks

--- tests/helper.js
+++ tests/helper.js
@@ -1,37 +1,84 @@
+export const mockResponse = {
+    mode: "analogic-complement",
+    count: "7",
+    colors: [
+        {
+            hex: { value: "#6F928B", clean: "6F928B" }
+        },
+        {
+            hex: { value: "#759E96", clean: "759E96" },
+        },
+        {
+            hex: { value: "#7CA9A1", clean: "7CA9A1" },
+        },
+        {
+            hex: { value: "#B8AD86", clean: "B8AD86" },
+        },
+        {
+            hex: { value: "#B5BA88", clean: "B5BA88" },
+        },
+        {
+            hex: { value: "#A6BC89", clean: "A6BC89" },
+        },
+        {
+            hex: { value: "#98BE8B", clean: "98BE8B" },
+        },
+    ],
+    seed: {
+        hex: { value: "#B98790", clean: "B98790" },
+        name: {
+            value: "Brandy Rose",
+            closest_named_hex: "#BB8983",
+            exact_match_name: false,
+            distance: 519,
+        },
+    }
+};
+
+export const mockUrl = "https://www.thecolorapi.com/scheme?hex=B98790&mode=analogic-complement&count=7";
+
 export const mockColorArr = [
-    "#000000", 
-    "#000000", 
-    "#000000", 
-    "#000000", 
-    "#000000", 
-    "#000000"
-]
+    "#6F928B",
+    "#759E96",
+    "#7CA9A1",
+    "#B8AD86",
+    "#B5BA88",
+    "#A6BC89",
+    "#98BE8B",
+];
+
+export const mockSchemeDetails = {
+    rootColor: "#B98790",
+    rootColorName: "Brandy Rose",
+    colorMode: "analogic-complement",
+};
 
 export const mockScheme = {
-    "a": "#000000",
-    "article": "#000000",
-    "aside": "#000000",
-    "body": "#000000",
-    "button": "#000000",
-    "code": "#000000",
-    "div": "#000000",
-    "footer": "#000000",
-    "h1": undefined,
-    "h2": "#000000",
-    "h3": "#000000",
-    "h4": "#000000",
-    "h5": "#000000",
-    "h6": "#000000",
-    "header": "#000000",
-    "li": "#000000",
-    "main": "#000000",
-    "nav": "#000000",
-    "p": undefined,
-    "pre": "#000000",
-    "section": "#000000",
-    "span": undefined,
-    "tbody": "#000000",
-    "td": "#000000",
-    "th": "#000000",
-    "ul": "#000000",
-}
+    "a": "#A6BC89",
+    "article": "#B8AD86",
+    "aside": "#B8AD86",
+    "body": "#6F928B",
+    "button": "#7CA9A1",
+    "code": "#759E96",
+    "div": "#6F928B",
+    "footer": "#B8AD86",
+    "h1": "#98BE8B",
+    "h2": "#A6BC89",
+    "h3": "#A6BC89",
+    "h4": "#B5BA88",
+    "h5": "#B5BA88",
+    "h6": "#B5BA88",
+    "header": "#B8AD86",
+    "li": "#B5BA88",
+    "main": "#6F928B",
+    "nav": "#B8AD86",
+    "p": "#98BE8B",
+    "pre": "#759E96",
+    "section": "#B8AD86",
+    "span": "#98BE8B",
+    "tbody": "#B8AD86",
+    "td": "#7CA9A1",
+    "th": "#7CA9A1",
+    "ul": "#A6BC89",
+    schemeDetails: mockSchemeDetails,
+};
